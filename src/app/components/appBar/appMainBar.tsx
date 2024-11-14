88a1b2244f6562ef6f51a35649bfd88ecd73f98e
Ivan Ramos <ivan1900@gmail.com>
2024-11-14 22:13:43 +0100

    feat: handle signout

--- src/app/components/appBar/appMainBar.tsx
+++ src/app/components/appBar/appMainBar.tsx
@@ -7,6 +7,8 @@ import {
   Typography,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { signOut, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 interface Props {
   drawerWidth: number;
@@ -16,28 +18,36 @@ interface Props {
 export default function AppMainBar(props: Props) {
   const { drawerWidth, handleDrawerToggle } = props;
 
+  const handleLogout = async () => {
+    await signOut();
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
-        position='fixed'
+        position="fixed"
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
-        }}>
+        }}
+      >
         <Toolbar>
           <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}>
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Daily notes
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Gazelle
           </Typography>
-          <Button color='inherit'>Login</Button>
+          <Button color="inherit" onClick={handleLogout}>
+            Salir
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
