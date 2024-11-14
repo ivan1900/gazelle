10ef68e71aa3a8e208da66b3b3cf7d41636c3c48
Ivan Ramos <ivan1900@gmail.com>
2024-11-14 22:00:21 +0100

    refactor: Create a private group(route) with two layouts one for handling session other por dashboard layout

---
+++ src/app/dashboard/(private)/layout.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import { useState } from 'react';
+import AppMainBar from '../../components/appBar/appMainBar';
+import SideMenu from '../../components/sideMenu/sideMenu';
+
+const drawerWidth = 240;
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
+
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
+
+  const handleDrawerToggle = () => {
+    console.log('handleDrawerToggle');
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+
+      <AppMainBar
+        drawerWidth={drawerWidth}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+
+      <SideMenu
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        handleDrawerClose={handleDrawerClose}
+        handleDrawerTransitionEnd={handleDrawerTransitionEnd}
+      />
+
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+}
