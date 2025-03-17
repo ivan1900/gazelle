f6635d0e11657f0d7780e7995d98ece01e4dbe69
Ivan Ramos <ivan1900@gmail.com>
2025-03-17 22:00:51 +0100

    fix: layout

--- src/app/dashboard/(private)/layout.tsx
+++ src/app/dashboard/(private)/layout.tsx
@@ -34,7 +34,7 @@ export default function DashboardLayout({
   };
 
   return (
-    <Box sx={{ display: 'block' }}>
+    <Box display={{ xs: 'block', md: 'flex' }}>
       <CssBaseline />
 
       <AppMainBar
